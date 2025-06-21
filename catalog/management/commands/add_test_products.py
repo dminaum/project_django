58febe719557d3e3c8e0edd2960a0885c77e296f
Дмитрий Наумов <dmi3naumov@gmail.com>
2025-06-21 20:16:49 +0300

    я ненавижу кодировку windows 1984 и желаю ей зла

--- catalog/management/commands/add_test_products.py
+++ catalog/management/commands/add_test_products.py
@@ -1,33 +1,17 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-    help = 'Удаляет старые и добавляет тестовые продукты'
+    help = 'Очищает БД и загружает данные из фикстур'
 
     def handle(self, *args, **kwargs):
         Product.objects.all().delete()
         Category.objects.all().delete()
+        self.stdout.write(self.style.WARNING('Старые данные удалены'))
 
-        self.stdout.write(self.style.WARNING('Все старые продукты и категории удалены.'))
+        call_command('loaddata', 'categories.json')
+        call_command('loaddata', 'products.json')
 
-        category = Category.objects.create(
-            name='Тестовая категория',
-            description='Описание тестовой категории'
-        )
-
-        products = [
-            {'name': 'Тестовый мяч', 'price': 10.99},
-            {'name': 'Тестовая футболка', 'price': 19.99},
-            {'name': 'Тестовый ноутбук', 'price': 999.99},
-        ]
-
-        for data in products:
-            product = Product.objects.create(
-                name=data['name'],
-                price=data['price'],
-                category=category
-            )
-            self.stdout.write(self.style.SUCCESS(f'Добавлен продукт: {product.name}'))
-
-        self.stdout.write(self.style.SUCCESS('Готово! Все тестовые продукты добавлены.'))
+        self.stdout.write(self.style.SUCCESS('Фикстуры успешно загружены'))
