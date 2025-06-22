2572bf4da9556a3a198aa6669cbf8015020cd067
Дмитрий Наумов <dmi3naumov@gmail.com>
2025-06-22 19:32:57 +0300

    я все еще ненавижу кодировку windows 1984 и желаю ей зла

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail')
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {'products': Product.objects.all()}
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
